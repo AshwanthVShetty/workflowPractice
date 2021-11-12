c8d0fc1de60daaf4851bc5256fa023f42e1a6a40
Ashwanth12022000 <vshettyashwanth@gmail.com>
2021-11-12 12:28:21 +0530

    3rd commit

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="timechecker",
+    name="AS_timechecker",
     version="0.0.5",
-    author="Edidiong Etuk",
-    author_email="edeediong@gmail.com",
-    url="https://bit.ly/edeediong-resume",
+    author="Ashwanth Shetty",
+    author_email="vshettyashwanth@gmail.com",
+    url="",
     description="An aplication that informs you of the time in different locations and timezones",
     packages=find_packages(),
     classifiers=[
